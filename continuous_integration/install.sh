dbdd07417af70a5482dd3843de95d250fba6f4f8
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2016-02-16 09:50:49 +0100

    specify exact version of wheel to install

--- continuous_integration/install.sh
+++ continuous_integration/install.sh
@@ -88,9 +88,9 @@ elif [[ "$DISTRIB" == "ubuntu" ]]; then
     pip install coverage
     # build wheels only if none present
     pip wheel --find-links=$HOME/.cache/pip/wheels --use-wheel --wheel-dir=$HOME/.cache/pip/wheels numpy==$NUMPY_VERSION
-    pip install --no-index --find-links=$HOME/.cache/pip/wheels numpy
+    pip install --no-index --find-links=$HOME/.cache/pip/wheels numpy==$NUMPY_VERSION
     pip wheel --find-links=$HOME/.cache/pip/wheels --use-wheel --wheel-dir=$HOME/.cache/pip/wheels scipy==$SCIPY_VERSION
-    pip install --no-index --find-links=$HOME/.cache/pip/wheels scipy
+    pip install --no-index --find-links=$HOME/.cache/pip/wheels scipy==$SCIPY_VERSION
     pip install six
     pip install quantities
 fi
